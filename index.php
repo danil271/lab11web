c8615df02b031782b1e80b12b602cd51b1c09992
danil271 <81598231+danil271@users.noreply.github.com>
2021-06-21 21:56:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?= $this->include('template/header'); ?>
+<?php if($artikel): foreach($artikel as $row): ?>
+<article class="entry">
+ <h2<a href="<?= base_url('/artikel/' . $row['slug']);?>"><?=
+$row['judul']; ?></a>
+</h2>
+ <img src="<?= base_url('/gambar/' . $row['gambar']);?>" alt="<?=
+$row['judul']; ?>">
+ <p><?= substr($row['isi'], 0, 200); ?></p>
+</article>
+<hr class="divider" />
+<?php endforeach; else: ?>
+<article class="entry">
+ <h2>Belum ada data.</h2>
+</article>
+<?php endif; ?>
+<?= $this->include('template/footer'); ?>
+INSERT INTO artikel (judul, isi, slug) VALUE
+('Artikel pertama', 'Lorem Ipsum adalah contoh teks atau dummy dalam industri
+percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi
+standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak
+dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah
+buku contoh huruf.', 'artikel-pertama'),
+('Artikel kedua', 'Tidak seperti anggapan banyak orang, Lorem Ipsum bukanlah
+teks-teks yang diacak. Ia berakar dari sebuah naskah sastra latin klasik dari
+era 45 sebelum masehi, hingga bisa dipastikan usianya telah mencapai lebih
+dari 2000 tahun.', 'artikel-kedua');
+public function view($slug)
